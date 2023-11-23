89ae7d1ec9de0467633c0bb12060901ce9ef9b81
sandy619g <sandy619g@gmail.com>
2023-11-23 23:29:21 +0530

    refactored decimal to roman conversion

--- src/main/java/com/app/converter/utils/ConverterUtil.java
+++ src/main/java/com/app/converter/utils/ConverterUtil.java
@@ -3,24 +3,23 @@ package com.app.converter.utils;
 import com.app.converter.exception.NumeralException;
 
 public final class ConverterUtil {
-    private static final String[] ROMAN_SYMBOLS = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-    private static final int[] VALUES = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    public static final String m[] = { "", "M", "MM", "MMM" };
+    public static final String c[] = { "",  "C",  "CC",  "CCC",  "CD",
+            "D", "DC", "DCC", "DCCC", "CM" };
+    public static final String x[] = { "",  "X",  "XX",  "XXX",  "XL",
+            "L", "LX", "LXX", "LXXX", "XC" };
+    public static final String i[] = { "",  "I",  "II",  "III",  "IV",
+            "V", "VI", "VII", "VIII", "IX" };
 
     public static String getDecimalToRomanConversion(int num) {
-        if (num <= 0) {
-            throw new NumeralException("Input must be a positive integer.");
+        if (num <= 0 || num > 3999) {
+            throw new NumeralException("Numeral should be within range 1-3999");
         }
-        StringBuilder result = new StringBuilder();
-        int i = VALUES.length - 1;
-        while (num > 0) {
-            if (num >= VALUES[i]) {
-                result.append(ROMAN_SYMBOLS[i]);
-                num -= VALUES[i];
-            } else {
-                i--;
-            }
-        }
-        return result.toString();
+        String thousands = m[num / 1000];
+        String hundreds = c[(num % 1000) / 100];
+        String tens = x[(num % 100) / 10];
+        String ones = i[num % 10];
+        return thousands + hundreds + tens + ones;
     }
 
 }
